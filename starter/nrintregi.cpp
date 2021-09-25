843190d07664b43621118f0020b40f6a91764bf5
constyy <constyadvleet@gmail.com>
2021-09-25 18:21:43 +0300

    Added starters

---
+++ starter/nrintregi.cpp
@@ -0,0 +1,8 @@
+#include <bits/sdc++.h>
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << x+y+z;
+}
---
+++ starter/nrnaturale.cpp
@@ -0,0 +1,8 @@
+#include <bits/sdc++.h>
+using namespace std;
+
+int main() {
+    unsigned x, y, z;
+    cin >> x >> y >> z;
+    cout << x+y+z;
+}
