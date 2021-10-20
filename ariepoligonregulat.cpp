24709a36d6133e4dc82e80ec6ef261bd0cc45fe0
constyy <constyadvleet@gmail.com>
2021-10-20 10:52:56 +0300

    added some problems

---
+++ ariepoligonregulat.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// unfinished (3803)
+int main() {
+   int n;
+   double l, apotema, aria;
+   cin >> n >> l;
+
+   aria = n*l*(l/2 * tan(3.14159*(n-2)/2/n))/2;
+   aria *= 100;
+
+   cout << int(aria/100) << '.' << int(aria/10)%10 << int(aria)%10;
+   return 0;
+}
+// unfinished (3803)
---
+++ unghiuri-adiacente.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << (180-x);
+    return 0;
+}
